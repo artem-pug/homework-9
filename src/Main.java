d03e9dfa9472036ac8ad9bfcdf002b72635681db
79535 <pugaart1@mail.ru>
2025-05-06 22:52:16 +0300

    Homework 9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Задание 1");
+        int[] payments = {10343, 12945, 15637, 11823, 13672};
+        int sum = 0;
+        for (int payment : payments) {
+            sum += payment;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+        System.out.println("Задание 2");
+        int maxPayment = -1;
+        for (int payment : payments) {
+            if (payment > maxPayment) {
+                maxPayment = payment;
+            }
+        }
+
+        int minPayment = 1000000;
+        for (int payment : payments) {
+            if (payment < minPayment) {
+                minPayment = payment;
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + minPayment + " рублей. " +
+                "Максимальная сумма трат за неделю составила " + maxPayment + " рублей.");
+        System.out.println("Задание 3");
+        int weeks = 5;
+        float averageAmount = 0;
+        for (int payment : payments) {
+            sum += payment;
+        }
+        averageAmount = (float) sum / weeks;
+        System.out.println(averageAmount);
+        System.out.println("Задание 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
